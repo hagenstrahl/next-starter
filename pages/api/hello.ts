89c178c4924ee671bd070acb1058811d6385ade1
Hagen Strahl <34143718+hagenstrahl@users.noreply.github.com>
2021-12-06 21:17:38 +0100

    update all dependencies (#123)
    * update all dependencies
    
    * add linting workflow
    
    * fix stylelint
    
    * remove babelrc to use swc compiling
    
    * fix jest
    
    * update nextjs

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default (req: NextApiRequest, res: NextApiResponse): void => {
+const hello = (req: NextApiRequest, res: NextApiResponse): void => {
   res.status(200).json({ name: "John Doe" });
 };
+
+export default hello;
